edd46964ec3a8cdd4828086844efdb56a0d5246b
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-09 18:41:12 -0400

    questao 2

---
+++ prova02_concatenar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace prova02_concatenar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            for (int i = 1; i <= 3; i++ ){
+                Console.WriteLine("Informe nome ou sobrenome");
+                nome += Console.ReadLine() + " ";
+            }
+
+            Console.WriteLine(nome);
+        }
+    }
+}
